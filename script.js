d660c0f5c70a5c557be7f28e31d7e7a3207aa4ab
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-02-17 07:18:19 +0800

    done!

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strick";
+
+const container = document.querySelector(".container");
+const clickBtn = document.querySelector(".cl-btn");
+clickBtn.addEventListener("click", inputNum);
+
+function inputNum() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  const num = prompt("How many grid do you want?", "<100");
+
+  container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  makeGrids(num);
+}
+
+function makeGrids(n) {
+  for (i = 0; i < n * n; i++) {
+    let divs = document.createElement("div");
+    divs.classList.add(`grid`);
+    container.appendChild(divs);
+    divs.style.opacity = 0;
+  }
+
+  const grids = container.querySelectorAll(".grid");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseenter", paint);
+  });
+}
+
+function paint() {
+  let opa = Number(window.getComputedStyle(this).getPropertyValue("opacity"));
+  if (opa < 1) {
+    opa = opa + 0.1;
+    this.style.opacity = opa;
+  }
+  this.style.background = `rgb(0, 0, 0)`;
+}
