0384647ce5f6e0b8243dd442f3f48344e71d5701
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-03-19 22:33:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ function inputNum() {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
-  const num = prompt("How many grids do you want?", "<100");
+  const num = prompt("How many grids do you want? (30 < grids < 80)");
+
+  // if (!num || num < 30 || num > 100) return alert("Invalid number!");
 
   container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
   makeGrids(num);
@@ -19,7 +21,7 @@ function makeGrids(n) {
     let divs = document.createElement("div");
     divs.classList.add(`grid`);
     container.appendChild(divs);
-    divs.style.opacity = 0;
+    divs.style.opacity = 0.1;
   }
   const grids = container.querySelectorAll(".grid");
   grids.forEach((grid) => {
@@ -28,11 +30,12 @@ function makeGrids(n) {
 }
 
 function paint() {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  // const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   let opa = Number(window.getComputedStyle(this).getPropertyValue("opacity"));
   if (opa < 1) {
-    opa += 0.1;
+    opa += 0.15;
     this.style.opacity = opa;
-    this.style.background = `#${randomColor}`;
+    this.style.background = "black";
+    // this.style.background = `#${randomColor}`;
   }
 }
